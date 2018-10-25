edc628a93b74ce68af38dabc46549cce6db5fb7c
JulieCCe <julie.chane@gmail.com>
2018-10-25 23:07:59 +0200

    créer le tableau et placer le fruit

--- game.js
+++ game.js
@@ -1,39 +1,57 @@
 // Paramètres du jeu
-var boardDim = 2; // dimensions du tableau
+var boardDim = 10; // dimensions du tableau
 var snakeFreq; // fréquence à laquelle le serpent grandit
 var bonusFruit; // nombre de points gagnés à chaque fruit croqué
 
-// create the board
-var board = [];
-
+// create board
 function createBoard(boardDim) {
-    var boardTable = `<table class="board">`;
-    var boardRow = `<tr class="{ligne-ID}">`;
-    var boardCell = `<td class="{ligneID} {columnID}" ></td> `;
- 
+  var boardTable = `<table class="board">`;
+  var boardRow = `<tr class="{ligne-ID}">`;
+  var boardCell = `<td class="{ligneID} {columnID}" ></td> `;
+
   for (var i = 0; i < boardDim; i++) {
-    boardRow = boardRow.replace("{ligne-ID}", "row" + i);
+    var myBoardRow = boardRow.replace("{ligne-ID}", "row" + i);
 
     for (var j = 0; j < boardDim; j++) {
-        console.log("j= "+j);
-      boardCell = boardCell.replace("{ligneID}", "row" + i+"");
-      boardCell = boardCell.replace("{columnID}", "col" + j+"");
-      boardRow = boardRow + boardCell;
-      console.log("j= "+j);
+    //   console.log("i=" + i + ", j= " + j);
+      var rowtext = "row" + i;
+      var coltext = "col" + j;
+      var myBoardCell = boardCell.replace("{ligneID}", rowtext);
+      myBoardCell = myBoardCell.replace("{columnID}", coltext);
+    //   console.log("myboardCell=" + myBoardCell);
+      myBoardRow += myBoardCell;
     }
-    boardRow = boardRow + `</tr>`;
-    console.log(i);
-    boardTable = boardTable + boardRow;
+    myBoardRow += "</tr>" + "\n";
+    boardTable += myBoardRow;
   }
   boardTable = boardTable + `</table>`;
-  console.log("Ligne finale : " + boardTable);
+//   console.log("Ligne finale:\n" + boardTable);
+  return boardTable;
 }
+
+// Initialiser le jeu : afficher le tableau et le fruit
 var startGame = function() {
-  createBoard(boardDim);
+  var boardDiv = document.getElementById("board-game");
+  boardDiv.innerHTML = createBoard(boardDim);  
+  $('#board-game tr .' + fruitClass).addClass('fruit');
 };
+
+// placer le fruit au hasard
+var fruit = {
+  x : Math.floor(Math.random() * (boardDim +1 )),
+  y : Math.floor(Math.random() * (boardDim +1 )),
+};
+// récupérer la position du fruit
+var fruitPosition = {
+    row : "row" + fruit.x,
+    col : "col"+fruit.y,
+  };
+var fruitClass = fruitPosition.row + "." + fruitPosition.col;
 
 $(document).ready(function() {
   $(".button-start-game").click(function() {
     startGame();
+    console.log(fruitClass);
+    // $('#board-game tr .row1.col2').addClass('fruit');
   });
 });
